b2e3390e7cf0ba982cfce27d1e05662aacc140ba
Igorechkere <igorshvedenko@yandex.ru>
2026-02-02 13:40:48 +0300

    clear code

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-// const reg = document.getElementById('butLog');
-// console.log(reg)
-// const container = document.querySelector('.container')
-// console.log(container)
-// reg.addEventListener('click', (e) => {
-//     container.classList.add('show-register');
-// });
-
 const log = document.getElementById('log');
 const reg = document.getElementById('reg');
 const container = document.querySelector('.container');
 const eye = document.querySelectorAll('#eye');
 const pas = document.querySelectorAll('.pas');
-console.log(eye)
+
 container.addEventListener('mousedown', (event) => {
     if (!event.target.dataset.action) return
     const inpPas = event.target.previousElementSibling;
@@ -22,12 +14,6 @@ container.addEventListener('mouseup', (event) => {
     const inpPas = event.target.previousElementSibling;
     inpPas.setAttribute('type', 'password');
 })
-// eye.addEventListener('mousedown', () => {
-//     pas.setAttribute('type', 'text');
-// })
-// eye.addEventListener('mouseup', () => {
-//     pas.setAttribute('type', 'password');
-// })
 reg.addEventListener('click', () => {
     container.classList.add('show-register');
 })
